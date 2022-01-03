c789c4c00daf44d92d18fe604321bc8482dfc275
jpretty01 <jeremypretty10@gmail.com>
2022-01-03 07:47:18 -0600

    Etch A Sketch
    Etch A Sketch

---
+++ main.py
@@ -0,0 +1,36 @@
+""" Etch-A-Sketch"""
+
+# Import the Turtle items needed
+from turtle import Turtle, Screen
+
+# Creating our Turtle character
+tim = Turtle()
+screen = Screen()
+
+# Fuctions to move all directions
+def move_forwards():
+    tim.forward(10)
+
+def move_backwards():
+    tim.backward(10)
+
+def turn_left():
+    tim.left(10)
+
+def turn_right():
+    tim.right(10)
+
+# Clear the screen and start over
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+screen.listen()
+screen.onkey(move_forwards, "w")
+screen.onkey(move_backwards, "s")
+screen.onkey(turn_left, "a")
+screen.onkey(turn_right, "d")
+screen.onkey(clear, "c")
+screen.exitonclick()
